dafc3a3da6d81e8eeddb1047e1b77aa316b883fa
joni115 <Jonathan.Mutal@unige.ch>
2019-09-19 16:53:32 +0200

    adding new corpus

--- utils.py
+++ utils.py
@@ -1,31 +1,49 @@
 import glob
+import os
+
 
 from gensim.utils import simple_preprocess
 from zipfile import ZipFile
 
-
 def pre_proccess_line(line):
     return simple_preprocess(line.strip())
 
-def read_zip(zipfile='cookbook_text.zip'):
+def read_zip(zipfile='text/cookbook_text.zip'):
     zipfile = ZipFile(zipfile, 'r')
     for filename in zipfile.namelist():
         text = zipfile.read(filename)
         yield pre_proccess_line(text)
 
-def read_files_in_directory(directory='shakespeare'):
+def read_file(filename='text'):
+    with open(filename, 'r') as f:
+        text = f.read()
+    yield text
+
+def read_files_in_directory(directory='text/shakespeare'):
     files = glob.glob(directory + '/*.txt')
     for filename in files:
-        with open(filename, 'r') as f:
-            text = f.read()
-        yield pre_proccess_line(text)
+        read_file(filename)
 
 def load_corpus(corpus=''):
-    switcher = {
-        'cookbook': read_zip(),
-        'shakespeare': read_files_in_directory(),
-    }
+    global switcher
+
     if corpus in switcher.keys():
         return switcher[corpus]
     else:
         raise NameError
+
+def add_corpus(corpus, path):
+    global switcher
+
+    if not os.path.isfile(path):
+        print('{} is not a valid file'.format(path))
+        return
+
+    switcher[corpus] = read_file(path)
+    print('corpus added.')
+
+switcher = {
+    'cookbook': read_files_in_directory('text/cookbook_text'),
+    'shakespeare': read_files_in_directory(),
+    'wikipedia': read_files_in_directory('text/wikipedia')
+}
